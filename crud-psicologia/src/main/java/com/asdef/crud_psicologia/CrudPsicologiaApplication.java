820c0f29422352bffb20846f44579f6961b9c107
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-06 14:55:13 -0300

    Criação do projeto

---
+++ crud-psicologia/src/main/java/com/asdef/crud_psicologia/CrudPsicologiaApplication.java
@@ -0,0 +1,15 @@
+package com.asdef.crud_psicologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPsicologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPsicologiaApplication.class, args);
+	}
+
+	
+
+}
---
+++ crud-psicologia/src/test/java/com/asdef/crud_psicologia/CrudPsicologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdef.crud_psicologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPsicologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
